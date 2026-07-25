ff94bc5f9047f58efd3867c96b24a2b9e30a45a3
Vrinda Angira <angiravrinda@gmail.com>
2026-07-25 17:07:04 +0530

    Fix backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: "https://student-management-system-r2oq.onrender.com/api",
 });
 
 // Automatically attach JWT token
