9272a5d4143fbd4767710f5128833cea119c8929
Vrinda Angira <angiravrinda@gmail.com>
2026-07-30 12:41:56 +0530

    Fix login API and deployment issues

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:  import.meta.env.VITE_API_URL,
+    baseURL:"https://charming-flow-production-3ddf.up.railway.app/" ,
 });
 
 api.interceptors.request.use((config) => {
