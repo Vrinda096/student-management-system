c48e5111cd6384eaf78f2a0a269b4485f030bb0e
Vrinda Angira <angiravrinda@gmail.com>
2026-07-30 16:36:08 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://charming-flow-production-3ddf.up.railway.app" ,
+    baseURL: "https://charming-flow-production-3ddf.up.railway.app/api" ,
 });
 
 api.interceptors.request.use((config) => {
