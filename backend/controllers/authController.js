572e07e62807576f2e63ce13e37752b7af4acbfd
Vrinda Angira <angiravrinda@gmail.com>
2026-08-05 01:35:02 +0530

    login issue

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 // Register User
 const registerUser = async (req, res) => {
     try {
-        const { name, email, password, role } = req.body;
+        const { name, email, password } = req.body;
 
         if (!name || !email || !password) {
             return res.status(400).json({
@@ -27,7 +27,6 @@ const registerUser = async (req, res) => {
     name,
     email,
     password: hashedPassword,
-    role: role || "student"
 });
         await user.save();
 
@@ -71,22 +70,16 @@ const loginUser = async (req, res) => {
             });
         }
 
-        const token = jwt.sign(
-    {
-        id: user._id,
-        role: user.role
-    },
+
     process.env.JWT_SECRET,
     {
         expiresIn: "1d"
     }
-);
+
         
 
         res.status(200).json({
-    token,
-    role: user.role,
-    user
+    token
 });
 
     } catch (error) {
