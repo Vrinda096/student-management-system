f2064a02fa55b33ea6776d9caae5cc4d85e533ab
Vrinda Angira <angiravrinda@gmail.com>
2026-08-09 21:47:43 +0530

    collapsable sidebar

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,27 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: "https://charming-flow-production-3ddf.up.railway.app/api" ,
+// });
+
+// api.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+// });
+
+// export default api;
+
+
+
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://charming-flow-production-3ddf.up.railway.app/api" ,
+    baseURL: "https://charming-flow-production-3ddf.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
